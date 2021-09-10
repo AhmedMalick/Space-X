98b3fcc28d3ddb48c069660b178f794187ac81d7
AhmedMalick <ahmedmalik2k20@gmail.com>
2021-09-10 12:28:23 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,19 +3,17 @@ const version = "v1";
 const cacheFiles = [
   "https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed&display=swap",
   "/static/media/D-DIN-Bold.7bb4b1a4.otf",
-  "/static/css/main.932b94d9.chunk.css",
-  '/static/js/main.a17aeb32.chunk.js',
-  "/static/js/main.e8c49f3a.chunk.js",
-  "/static/media/video.023e76fa.mp4",
+  "/static/css/main.e8218c26.chunk.css",
+  "/static/js/main.fdde8e10.chunk.js",
   "/static/media/D-DIN.31d45669.otf",
   "/static/media/bg-1.0da36e21.jpg",
   "/static/media/bg-2.ab56f8ec.jpg",
   "/static/media/bg-3.6d3b42c3.jpg",
   "/static/media/bg-4.da6056dc.jpg",
-  "/static/js/2.3a42b226.chunk.js",
+  "/static/js/2.1cbda3d1.chunk.js",
   "/manifest.json",
-  "/favicon.ico",
   "/logo192.png",
+  "/favicon.ico",
   "/",
 ];
 
