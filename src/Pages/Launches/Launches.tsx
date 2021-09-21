235788932a64bad1f00283522a3af1c03e02a6ed
AhmedMalick <ahmedmalik2k20@gmail.com>
2021-09-21 16:08:06 +0500

    updated

--- src/Pages/Launches/Launches.tsx
+++ src/Pages/Launches/Launches.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import ReactPlayer from "react-player";
 import { Unnamed_1_Query } from "../../generated/graphql";
 import styles from "./Launches.module.css";
+
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+} from "@material-ui/lab";
 
 interface Props {
   data: Unnamed_1_Query | undefined;
@@ -13,49 +21,85 @@ const Launches: FC<Props> = ({ data }) => {
     <div className={styles.launches}>
       <h1>Launches</h1>
       <div className={styles.container}>
-        {data?.launches?.map((launch, index) => (
-          <div className={styles.launch} key={index}>
-            <Link className={styles.mission_name} to={`/launch/${launch?.id}`}>
-              {launch?.mission_name}
-            </Link>
-            {launch?.links?.video_link ? (
-              <ReactPlayer
-                className={styles.video}
-                data-testid="react-player"
-                light={true}
-                controls={true}
-                url={launch?.links?.video_link}
-              />
-            ) : null}
-            <p className={styles.details}>{launch?.details}</p>
-            <div className={styles.launchDetails}>
-              <p>
-                <span>LAUNCH DATE:</span>
-                {launch?.launch_date_local}
-              </p>
-              <p>
-                <span>LAUNCH SITE:</span>
-                {launch?.launch_site?.site_name_long}
-              </p>
-              <p>
-                <span>ROCKET:</span>
-                {launch?.rocket?.rocket_name}
-              </p>
-              <p>
-                <span>STATUS:</span>
-                {launch?.launch_success ? "Successful" : "Failed"}
-              </p>
-            </div>
-            <Link to={`/launch/${launch?.id}`}>
-              <div className={styles.btn}>
-                <span>MORE DETAILS</span>
-              </div>
-            </Link>
-          </div>
-        ))}
+        <Timeline align="alternate">
+          {data?.launches?.map((launch, index) => (
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <div className={styles.launch} key={index}>
+                  <Link
+                    className={styles.mission_name}
+                    to={`/launch/${launch?.id}`}
+                  >
+                    {launch?.mission_name}{" "}
+                  </Link>
+                  <p className={styles.details}>{launch?.details}</p>{" "}
+                  <div className={styles.launchDetails}>
+                    {" "}
+                    <p>
+                      <span>LAUNCH DATE:</span>
+                      {launch?.launch_date_local}{" "}
+                    </p>{" "}
+                    <p>
+                      <span>LAUNCH SITE:</span>
+                      {launch?.launch_site?.site_name_long}{" "}
+                    </p>{" "}
+                    <p>
+                      <span>ROCKET:</span>
+                      {launch?.rocket?.rocket_name}{" "}
+                    </p>{" "}
+                    <p>
+                      <span>STATUS:</span>
+                      {launch?.launch_success ? "Successful" : "Failed"}{" "}
+                    </p>{" "}
+                  </div>{" "}
+                  <Link to={`/launch/${launch?.id}`}>
+                    {" "}
+                    <div className={styles.btn}>
+                      <span>MORE DETAILS</span>{" "}
+                    </div>{" "}
+                  </Link>
+                </div>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+        </Timeline>
       </div>
     </div>
   );
 };
 
 export default Launches;
+
+// <div className={styles.launch} key={index}>
+//   <Link className={styles.mission_name} to={`/launch/${launch?.id}`}>
+//     {launch?.mission_name}
+//   </Link>
+//   <p className={styles.details}>{launch?.details}</p>
+//   <div className={styles.launchDetails}>
+//     <p>
+//       <span>LAUNCH DATE:</span>
+//       {launch?.launch_date_local}
+//     </p>
+//     <p>
+//       <span>LAUNCH SITE:</span>
+//       {launch?.launch_site?.site_name_long}
+//     </p>
+//     <p>
+//       <span>ROCKET:</span>
+//       {launch?.rocket?.rocket_name}
+//     </p>
+//     <p>
+//       <span>STATUS:</span>
+//       {launch?.launch_success ? "Successful" : "Failed"}
+//     </p>
+//   </div>
+//   <Link to={`/launch/${launch?.id}`}>
+//     <div className={styles.btn}>
+//       <span>MORE DETAILS</span>
+//     </div>
+//   </Link>
+// </div>
